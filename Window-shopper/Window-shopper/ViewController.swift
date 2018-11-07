fb8a3013f083dbf4c7dec9e825c96ca546c53365
Dobs Totev <dobstotev@Dobss-MBP.lan>
2018-11-07 09:50:31 +0100

    Starting the project

---
+++ Window-shopper/Window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Window-shopper
+//
+//  Created by Dobs Totev on 07/11/2018.
+//  Copyright © 2018 Dobs Totev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
